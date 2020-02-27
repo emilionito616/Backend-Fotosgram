3f3bd4f04939ad25d0743aa754a76dbf9054226b
emilionito616 <emilionito9@gmail.com>
2020-02-27 17:11:02 -0600

    Servicio de Login de usuarios

--- dist/routes/usuario.js
+++ dist/routes/usuario.js
@@ -1,11 +1,56 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const usuario_model_1 = require("../models/usuario.model");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const userRoutes = express_1.Router();
-userRoutes.get('/prueba', (req, res) => {
-    res.json({
-        ok: true,
-        mensaje: 'Todo funciona bien!'
+//Login
+userRoutes.post('/login', (req, res) => {
+    const body = req.body;
+    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
+        if (err)
+            throw err;
+        if (!userDB) {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos'
+            });
+        }
+        if (userDB.compararPassword(body.password)) {
+            res.json({
+                ok: true,
+                token: 'AAAAAAHSDHASHDJASHDJASD'
+            });
+        }
+        else {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos ****'
+            });
+        }
+    });
+});
+//Crear un usuario
+userRoutes.post('/create', (req, res) => {
+    const user = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: bcrypt_1.default.hashSync(req.body.password, 10),
+        avatar: req.body.avatar
+    };
+    usuario_model_1.Usuario.create(user).then(userDB => {
+        res.json({
+            ok: true,
+            user: userDB
+        });
+    }).catch(err => {
+        res.json({
+            ok: false,
+            err
+        });
     });
 });
 exports.default = userRoutes;
--- routes/usuario.ts
+++ routes/usuario.ts
@@ -1,11 +1,55 @@
 import { Router, Request, Response } from 'express';
+import { Usuario } from '../models/usuario.model';
+import bcrypt from 'bcrypt';
 
 const userRoutes = Router();
 
-userRoutes.get('/prueba', ( req: Request, res: Response ) => {
-    res.json({
-        ok: true,
-        mensaje: 'Todo funciona bien!'
+//Login
+userRoutes.post('/login', (req: Request, res: Response) => {
+    const body = req.body;
+
+    Usuario.findOne({ email: body.email}, (err, userDB) => {
+        if (err) throw err;
+        if(!userDB){
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos'
+            });
+        }
+        
+        if (userDB.compararPassword(body.password)){
+            res.json({
+                ok: true,
+                token: 'AAAAAAHSDHASHDJASHDJASD'
+            });
+        } else {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos ****'
+            });
+        }
+    });
+});
+
+//Crear un usuario
+userRoutes.post('/create', ( req: Request, res: Response ) => {
+    const user = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        avatar: req.body.avatar
+    }
+
+    Usuario.create( user ).then( userDB => {
+        res.json({
+            ok: true,
+            user: userDB
+        });
+    }).catch( err => {
+        res.json({
+            ok: false,
+            err
+        });
     });
 });
 
